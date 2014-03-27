fc3ae34c74ff45e6c7704554542cd7a30ef4c3c2
Luchs <Luchs@euirc.eu>
2014-03-27 21:12:30 +0100

    Implement score reading

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+
+	"./mvnv"
+)
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+			panic(err)
+	}
+	defer session.Close()
+
+	scores := session.DB("mvnv").C("scores")
+
+	score := mvnv.Score{}
+	err = scores.Find(bson.M{"id": 124}).One(&score)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Score: %v\n", score)
+}
---
+++ mvnv/score.go
@@ -0,0 +1,10 @@
+package mvnv
+
+type Score struct {
+	Id int
+	Title string
+	Composer string
+	Publisher string
+	Tags []string
+	Year int
+}
